436529fbc3e5237f5d7b115027435334cdc9d258
KazumiWT <kazumiwatanabetarget@gmail.com>
2024-11-06 15:04:01 -0500

    2eme commit

--- src/main/java/edu/bdeb/a13/Main.java
+++ src/main/java/edu/bdeb/a13/Main.java
@@ -1,7 +1,17 @@
 package edu.bdeb.a13;
 
+/**
+ * test docs
+ *
+ */
+
 public class Main {
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
     }
 }
